45f5ee221835a64f2d0048f169a8e8905b6fc028
Carolina Ruiz <Carolina.Ruiz@FutureElectronics.com>
2025-11-22 10:24:34 -0500

    fix: change the number of columns in mobile screen

--- app/components/SkillsSection.jsx
+++ app/components/SkillsSection.jsx
@@ -259,7 +259,7 @@ const SkillsSection = () => {
             <div>
                 <h6>Core Stack</h6>
 
-                <div className="grid grid-cols-5 lg:grid-cols-8">   
+                <div className="grid grid-cols-4 lg:grid-cols-8">   
                     {
                     skillsData.map((skill, index) => (
                             skill.type === "core" ? (
@@ -278,7 +278,7 @@ const SkillsSection = () => {
             <div className='grid lg:grid-cols-1 gap-10 my-4'>
             <div>
                 <h6>Secondary</h6>
-                <div className="grid grid-cols-5 lg:grid-cols-8 mx-8">
+                <div className="grid grid-cols-4 lg:grid-cols-8 mx-8">
                     
                     {
                     skillsData.map((skill, index) => (
